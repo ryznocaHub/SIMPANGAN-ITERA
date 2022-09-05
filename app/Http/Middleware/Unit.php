d58547ee28aa7dcc78999b9cc42164d76770dd80
ryznocaHub <fikrihalim27@gmail.com>
2022-09-05 15:18:29 +0700

    Add model iterasi 1 unit, supplier, user

---
+++ app/Http/Middleware/Unit.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\Unit as ModelsUnit;
+use App\Models\User;
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class Unit
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (Auth::check()) {
+            $role = Auth::user()->role;
+            if ($role == User::ROLE_UNIT) {
+                return $next($request);
+            } else {
+                abort(403);
+            }
+        } else {
+            return redirect('login');
+        }
+    }
+}
---
+++ app/Models/User.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class User extends Authenticatable
+{
+    use HasApiTokens, HasFactory, Notifiable;
+    const ROLE_Head_UKPBJ       = 1;
+    const ROLE_PPK              = 2;
+    const ROLE_PP               = 3;
+    const ROLE_HPS_TEAM         = 4;
+    const ROLE_CONTRACT_TEAM    = 5;
+    const ROLE_TECHNICAL_TEAM   = 6;
+    const ROLE_UNIT             = 7;
+    
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'role',
+        'unit_id'
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+}
