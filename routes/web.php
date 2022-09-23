b63f95ceff045be296fa01f20d862bb4351912d0
ryznocaHub <fikrihalim27@gmail.com>
2022-09-23 19:27:15 +0700

    add new routes

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,11 @@
 <?php
 
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\DocumentsController;
 use App\Http\Controllers\ProcurementAccountController;
 use App\Http\Controllers\ProcurementItemController;
+use App\Http\Controllers\SupplierController;
+use App\Models\Supplier;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -31,26 +34,56 @@ Route::get('/', function () {
     return redirect()->route('login');
 })->name('home');
 
+Route::get('/cek', [DashboardController::class, 'cek'])->name('cek');
+
 Route::group(['middleware' => 'auth'], function (){
     // Route::get('/', [DashboardController::class, 'cek'])->name('dashboard.TimHPS');
     
-    Route::group(['middleware' => 'unit', 'prefix' => 'unit'],function(){
-        Route::resource('procurement',          ProcurementAccountController::class)->except('destroy');
-        Route::get('/dashboard',                [DashboardController::class, 'unit'])->name('dashboard.unit');
-    });
-    
-    Route::group(['middleware' => 'headUKPBJ', 'prefix' => 'admin'],function(){
+    Route::prefix('admin')->middleware('headUKPBJ')->name('admin.')->group(function(){
         Route::resource('procurement',          ProcurementAccountController::class)->only(['index']);
-        Route::get('/dashboard',                [DashboardController::class, 'headUKPBJ'])->name('dashboard.unit');
-    });
-    
-    Route::group(['middleware' => 'hps', 'prefix' => 'hps'],function(){
-        
+        Route::get('/dashboard',                [DashboardController::class, 'headUKPBJ'])->name('dashboard');
     });
 
-    Route::post('/procurement/updatestatus/{id}', [ProcurementAccountController::class, 'updateStatus'])->name('procurement.updatestatus');
-    Route::get('/dashboard/TimHPS', [DashboardController::class, 'TimHPS'])->name('dashboard.TimHPS');
-    Route::resource('item',ProcurementItemController::class);
+    Route::prefix('unit')->middleware('unit')->name('unit.')->group(function(){
+        Route::resource('procurement',          ProcurementAccountController::class)->except(['destroy']);
+        Route::resource('item',                 ProcurementItemController::class)->only(['update']);
+        Route::get('/dashboard',                [DashboardController::class, 'unit'])->name('dashboard');
+    });
+    
+    Route::prefix('hps')->middleware('hps')->name('hps.')->group(function(){
+        Route::get('/dashboard',                [DashboardController::class, 'hps'])->name('dashboard');
+        Route::resource('procurement',          ProcurementAccountController::class)->only(['index', 'show', 'update', 'edit']);
+        Route::post('updatehps/{id}',           [ProcurementItemController::class, 'updateHPS'])->name('item.updateHPS');
+        Route::get('boq/{id}',                  [DocumentsController::class, 'boq'])->name('document.boq');
+    });
+    
+    Route::prefix('ppk')->middleware('ppk')->name('ppk.')->group(function(){
+        Route::get('/dashboard',                [DashboardController::class, 'ppk'])->name('dashboard');
+        Route::resource('procurement',          ProcurementAccountController::class)->only(['index', 'show', 'update']);
+        Route::get('boq/{id}',                  [DocumentsController::class, 'boq'])->name('document.boq');
+    });
+    
+    Route::prefix('pp')->middleware('pp')->name('pp.')->group(function(){
+        Route::get('/dashboard',                [DashboardController::class, 'pp'])->name('dashboard');
+        Route::resource('procurement',          ProcurementAccountController::class)->only(['index', 'show', 'update']);
+        Route::get('boq/{id}',                  [DocumentsController::class, 'boq'])->name('document.boq');
+        // Route::resource('supplier',             Supplier::class)                    ->only(['create','store']);
+    });
+    
+    Route::prefix('kontrak')->middleware('kontrak')->name('kontrak.')->group(function(){
+        Route::get('/dashboard',                [DashboardController::class, 'kontrak'])->name('dashboard');
+        Route::resource('procurement',          ProcurementAccountController::class)->only(['index', 'show', 'update']);
+        Route::resource('supplier',             SupplierController::class);
+        Route::get('boq/{id}',                  [DocumentsController::class, 'boq'])->name('document.boq');
+    });
+    
+    Route::prefix('siren')->middleware('siren')->name('siren.')->group(function(){
+        Route::get('/dashboard',                [DashboardController::class, 'siren'])->name('dashboard');
+        Route::resource('procurement',          ProcurementAccountController::class)->only(['index', 'show', 'update']);
+    });
+    
+    Route::resource('item',ProcurementItemController::class)->only(['show']);
+    Route::get('/dashboard/TimHPS',             [DashboardController::class, 'TimHPS'])->name('dashboard.TimHPS');
 });
 
 
