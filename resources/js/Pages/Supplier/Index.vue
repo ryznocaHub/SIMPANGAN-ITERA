77708d5427479ba1c1328edde0b3c7e69649c154
ryznocaHub <fikrihalim27@gmail.com>
2022-11-03 18:55:43 +0700

    add empty condition

--- resources/js/Pages/Supplier/Index.vue
+++ resources/js/Pages/Supplier/Index.vue
@@ -1,8 +1,8 @@
 <template>
     <Master>
-        <Container >
+        <Container v-if="suppliers.length != 0" >
             <Header1 title="Daftar Supplier" widthSize="100" />
-            <Link :href="route('kontrak.supplier.create')" class="btn btn-sm mt-5 mb-5 text-first bg-slate-200 border-none hover:text-white hover:bg-first  font-bold" >Tambah Data Supplier</Link>
+            <Link v-if="user.role > 4" :href="route('kontrak.supplier.create')" class="btn btn-sm mt-5 mb-5 text-first bg-slate-200 border-none hover:text-white hover:bg-first  font-bold" >Tambah Data Supplier</Link>
             <EasyDataTable :headers="headers" :items="suppliers" buttons-pagination>
                 <template #item-province="{ province }">
                     <div v-if="province">{{province}}</div>
@@ -24,11 +24,17 @@
                     <div v-else class="text-secondary">Belum Lengkap</div>
                 </template>
                 <template #item-aksi="{ entity,id }">
-                    <Link v-if="entity" :href="route('kontrak.supplier.show', id)" class="btn btn-xs btn-outline text-first font-bold" >Lihat</Link>
+                    <Link v-if="entity && user.role==1" :href="route('admin.supplier.show', id)" class="btn btn-xs btn-outline text-first font-bold" >Lihat</Link>
+                    <Link v-else-if="entity" :href="route('kontrak.supplier.show', id)" class="btn btn-xs btn-outline text-first font-bold" >Lihat</Link>
                     <Link v-else :href="route('kontrak.supplier.edit', id)" class="btn btn-xs text-white bg-first border-none font-bold" >Lengkapi Data</Link>
                 </template>
             </EasyDataTable>
         </Container>
+        <div v-else>
+            <EmptyPage text="supplier"/>
+            <!-- <Vue3Lottie :animationData="Empty" :height="450" :width="700" /> -->
+            <!-- <div class="text-x drop-shadow text-2xl font-bold text-first text-center">Maaf, Belum ada data supplier yang tersedia</div> -->
+        </div>
     </Master>
 </template>
 
@@ -37,7 +43,11 @@ import Master from "@/Layouts/Master.vue";
 import { Link } from '@inertiajs/inertia-vue3'
 import Container from "@/Components/utils/Container.vue";
 import Header1 from "@/Components/utils/Header1.vue";
+import EmptyPage from "@/Components/StatusPage/Empty.vue";
+import { computed } from 'vue'
+import { usePage } from '@inertiajs/inertia-vue3'
 
+const user = computed(() => usePage().props.value.auth.user)
  defineProps({
 	suppliers: { type: Object, required: true },
 })
