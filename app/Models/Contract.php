545882aebd2a68babea168b7df6cee6a1aa02416
ryznocaHub <fikrihalim27@gmail.com>
2022-11-15 21:38:45 +0700

    fix model contract, timeline

--- app/Models/Contract.php
+++ app/Models/Contract.php
@@ -13,7 +13,10 @@ class Contract extends Model
         'offer',
         'date_offer',
         'no_offer',
+        'file_offer',
+
         'no_spk',
+        'file_spk',
         'mak_code',
         'pic',
         'pic_position',
@@ -21,19 +24,38 @@ class Contract extends Model
         'days',
         'date_start_spk',
         'date_end_spk',
+
         'no_sppbj',
+        'file_sppbj',
         'date_sppbj',
         'purpose_sppbj',
+
         'no_sp',
+        'file_sp',
         'date_sp',
         'paket_sp',
         'no_report',
         'payment',
+
         'no_bastp',
+        'file_bastp',
         'date_bastp',
+
         'no_bahp',
+        'file_bahp',
         'date_bahp',
+
         'no_bap',
-        'date_bap',
+        'file_bap',
+        'date_bap', 
+
+        'no_bakn',
+        'file_bakn',
+        'date_bakn',
+
+        'no_baep',
+        'file_baep',
+        'date_baep',
+        'correction',
     ];
 }
